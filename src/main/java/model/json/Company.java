f7f56dfa080ce787ee0bfa96e40ccc2b6284f47f
dorian <dorian.glaeske@gmail.com>
2025-07-31 11:46:44 +0200

    Company angepasst an neue Json datei

--- src/main/java/model/json/Company.java
+++ src/main/java/model/json/Company.java
@@ -1,24 +1,37 @@
 package model.json;
 
+import java.util.List;
 
 /**
  * Company Klasse.
- * Spiegelt das json-Objekt wider.
- *
- * @author Dorian Gläske
- * @version 1.0
- * @since 2025-07-28
+ * Spiegelt das JSON-Objekt wider.
  */
 public class Company {
 
+    private String companyId;
     private String name;
+    private List<String> departments;
+    private Role role;
 
+    // Verwende deine vorhandene Container-Klasse
+    private List<QualificationsContainer> qualificationProfile;
 
-    public Company() {
+    public Company() {}
+
+    public Company(String companyId, String name, List<String> departments, Role role, List<QualificationsContainer> qualificationProfile) {
+        this.companyId = companyId;
+        this.name = name;
+        this.departments = departments;
+        this.role = role;
+        this.qualificationProfile = qualificationProfile;
     }
 
-    public Company(String name) {
-        this.name = name;
+    public String getCompanyId() {
+        return companyId;
+    }
+
+    public void setCompanyId(String companyId) {
+        this.companyId = companyId;
     }
 
     public String getName() {
@@ -29,4 +42,29 @@ public class Company {
         this.name = name;
     }
 
+    public List<String> getDepartments() {
+        return departments;
+    }
+
+    public void setDepartments(List<String> departments) {
+        this.departments = departments;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public List<QualificationsContainer> getQualificationProfile() {
+        return qualificationProfile;
+    }
+
+    public void setQualificationProfile(List<QualificationsContainer> qualificationProfile) {
+        this.qualificationProfile = qualificationProfile;
+    }
 }
+
+
